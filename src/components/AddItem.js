c64a4dcd60553057a95791ac5f296fd46dc96a00
Lillian Kunu <lillykunu>
2022-03-07 16:49:04 +0100

    Added new files and background picture

---
+++ src/components/AddItem.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function AddItem({ onAddItem }) {
+  const [item, setItem] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    onAddItem(item);
+    setItem("");
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor='new-item'>What else do you need? Add it here...</label>
+      <input
+        type='text'
+        id='new-item'
+        onChange={(event) => setItem(event.target.value)}
+        value={item}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
---
+++ src/components/ItemList.js
@@ -0,0 +1,5 @@
+import "./List.css";
+
+export default function List({ name }) {
+  return <li>{name}</li>;
+}
